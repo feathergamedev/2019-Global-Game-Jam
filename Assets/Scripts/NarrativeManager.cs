2becafa5d46b17ad955de110e8dd6c00ced01697
FeatherChung <feathergamedev@gmail.com>
2019-01-28 12:41:05 +0800

    初始上傳

---
+++ Assets/Scripts/NarrativeManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityAtoms;
+using TMPro;
+using DG.Tweening;
+
+public class NarrativeManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private TextMeshProUGUI Text_narrative;
+
+
+
+    [SerializeField]
+    private StringVariable cur_narrative;
+
+    private float cur_alpha;
+
+    Coroutine coroutine;
+    Tween tween;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cur_alpha = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text_narrative.color = new Color32(0, 0, 0, (byte)(cur_alpha * 255));
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            cur_narrative.Value = "Hi! Testing.";
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            cur_narrative.Value = "You can go anywhere.\nHave nowhere can go as well.";
+        }
+    }
+
+    public void Narrative_OnChange()
+    {
+        if (coroutine != null)
+            StopCoroutine(coroutine);
+
+        if (tween != null)
+            tween.Kill();
+
+        coroutine = StartCoroutine(Narrative_UI_Perform());
+    }
+
+    IEnumerator Narrative_UI_Perform()
+    {
+        tween = DOTween.To(() => cur_alpha, x => cur_alpha = x, 0, 0.5f);
+
+        yield return new WaitForSeconds(0.5f);
+
+        Text_narrative.text = cur_narrative.Value;
+
+        tween = DOTween.To(() => cur_alpha, x => cur_alpha = x, 1, 3.0f);
+
+
+        yield return null;
+    }
+
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+
+
+    private Rigidbody2D m_rigid;
+    private Vector2 m_velocity;
+
+    [SerializeField]
+    [Header("速度的倍率")]
+    private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));        
+    }
+
+    private void FixedUpdate()
+    {
+        m_rigid.velocity = m_velocity * moveSpeed;
+    }
+}
